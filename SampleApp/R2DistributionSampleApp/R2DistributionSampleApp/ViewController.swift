5c47b3e0f2a1ed6d6cdf147fe9d32b862be67e5a
Tamer Bader <tbader@squareup.com>
2024-05-15 16:25:26 -0700

    Skeleton Sample App

---
+++ SampleApp/R2DistributionSampleApp/R2DistributionSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R2DistributionSampleApp
+//
+//  Created by Tamer Bader on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

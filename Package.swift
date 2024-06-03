43700c981156f9aa59732dbd94e4e1f9d59b0380
Tamer Bader <tbader@squareup.com>
2024-06-03 11:16:10 -0700

    SPM coverage

--- Package.swift
+++ Package.swift
@@ -1,17 +1,23 @@
-// swift-tools-version:5.10
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "SquareMobilePaymentsSDK",
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "SquareMobilePaymentsSDK", targets: ["SquareMobilePaymentsSDK"]),
+        .library(name: "MockReaderUI", targets: ["MockReaderUI"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "SquareMobilePaymentsSDK",
-            path: "XCFrameworks/SquareMobilePaymentsSDK.xcframework.zip"
+            path: "XCFrameworks/SquareMobilePaymentsSDK_2c76b3183c4f.zip"
+        ),
+        .binaryTarget(
+            name: "MockReaderUI",
+            path: "XCFrameworks/MockReaderUI_2c76b3183c4f.zip"
         )
     ]
 )

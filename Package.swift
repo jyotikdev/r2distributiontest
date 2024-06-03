127b4c64d867809b206ec3c6d9d7c9a634ba8d33
Tamer Bader <tbader@squareup.com>
2024-06-03 13:52:20 -0700

    Specify platform version

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SquareMobilePaymentsSDK",
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "SquareMobilePaymentsSDK", targets: ["SquareMobilePaymentsSDK"]),
         .library(name: "MockReaderUI", targets: ["MockReaderUI"]),

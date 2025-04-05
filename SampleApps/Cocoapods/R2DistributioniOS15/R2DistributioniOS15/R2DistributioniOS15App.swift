4fb92b0301f44ece1b15379a2ce6316090b3e522
Tamer Bader <tbader@squareup.com>
2025-04-04 17:47:04 -0700

    Adding MPSDK test code to projects

--- SampleApps/Cocoapods/R2DistributioniOS15/R2DistributioniOS15/R2DistributioniOS15App.swift
+++ SampleApps/Cocoapods/R2DistributioniOS15/R2DistributioniOS15/R2DistributioniOS15App.swift
@@ -6,12 +6,40 @@
 //
 
 import SwiftUI
+import SquareMobilePaymentsSDK
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    var mobilePaymentSDK: SDKManager!
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        print("Hey")
+        MobilePaymentsSDK.initialize(
+            applicationLaunchOptions: launchOptions,
+            squareApplicationID: "sq0idp-2p247sPrashdnnY5BUu2AQ"
+        )
+        self.mobilePaymentSDK = MobilePaymentsSDK.shared
+        return true
+    }
+}
 
 @main
 struct R2DistributioniOS15App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+enum Config {
+    static let appID: String = ""
+    static let locationId: String = ""
+    static let accessToken: String = ""
 }
--- SampleApps/Cocoapods/R2DistributioniOS16/R2DistributioniOS16/ContentView.swift
+++ SampleApps/Cocoapods/R2DistributioniOS16/R2DistributioniOS16/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import SquareMobilePaymentsSDK
 
 struct ContentView: View {
+    
+    @State private var isAuthorized: Bool = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isAuthorized {
+                Text("Mobile Payments SDK: Authorized")
+            } else {
+                Text("Mobile Payments SDK: Unauthorized")
+            }
         }
         .padding()
+        .onAppear {
+            MobilePaymentsSDK.shared.authorizationManager.authorize(withAccessToken: Config.accessToken, locationID: Config.locationId) { error in
+                isAuthorized = (MobilePaymentsSDK.shared.authorizationManager.state == .authorized)
+            }
+        }
     }
 }
 

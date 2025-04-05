6a02f415d4fafa2fdd649d8e517f16404a8d4ff0
Tamer Bader <tbader@squareup.com>
2025-04-04 17:03:33 -0700

    Base iOS 15 Sample App

---
+++ SampleApps/Cocoapods/R2DistributioniOS15/R2DistributioniOS15/R2DistributioniOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  R2DistributioniOS15App.swift
+//  R2DistributioniOS15
+//
+//  Created by Tamer Bader on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct R2DistributioniOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
